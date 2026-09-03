48151071b0569b10609c39b3a61bec2245932b67
felipeborda-code <felipe_borda@estudante.sesisenai.org.br>
2026-09-03 15:43:24 -0300

    feat : express feito

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get("/", (req, res) => { 
+
+    res.json("Hello World")
+
+})
+
+app.listen(port, () =>  {
+
+    console.log(`Seja bem vindo a minha API: http://localhost:${port})`);
+
+})
